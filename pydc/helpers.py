605f1641baa834f9e3601267b4e325caf001b9e0
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-03-21 17:05:05 +0100

    Singleton namespace to hold types and validators
    The singleton object holds all the types and validators that can be
    used in a config file.  It also allows a config hook to add your own
    modules.

---
+++ pydc/helpers.py
@@ -0,0 +1,87 @@
+from importlib import import_module
+from types import SimpleNamespace
+from typing import Iterable
+
+
+class _Names(SimpleNamespace):
+    """This is a namespace class used to create and hold several namespaces
+
+    The sub-namespaces are properties of this class, and are instantiated on
+    first access.  The class attributes `_type_modules` and
+    `_validator_modules` are a list of modules.  On first access the
+    corresponding properties (`types` and `validators`) are populated with all
+    the names included in `__all__` in these modules.  This "convention" is
+    used to limit the names that are imported for the sake of namespace
+    pollution.  The different sets of modules are also seperated under
+    different sub-namespaces to reduce the chance of name collissions.
+
+    This class is not supposed to be accessed directly; instead the singleton
+    object instantiated below should be imported.
+
+    If you want to add your custom modules, they can be included by adding to
+    the list of modules _before_ accessing the sub-namespaces.
+
+    >>> isinstance(NS, SimpleNamespace)
+    True
+    >>> isinstance(NS.types, SimpleNamespace)
+    True
+    >>> isinstance(NS.validators, SimpleNamespace)
+    True
+
+    >>> hasattr(NS.types, 'List')  # from typing import List
+    True
+    >>> hasattr(NS.types, 'Literal')  # from typing_extensions import Literal
+    True
+    >>> hasattr(NS.types, 'conint')  # from pydantic.types import conint
+    True
+    >>> hasattr(NS.types, 'bool')  # from pydc.types import bool
+    True
+
+    # from pydc.validators import range_check
+    >>> hasattr(NS.validators, 'range_check')
+    True
+
+    """
+
+    _type_modules = (
+        "typing",
+        "typing_extensions",
+        "pydantic.types",
+        "pydc.types",
+    )
+    _validator_modules = ("pydc.validators",)
+
+    def _import(self, modules: Iterable[str]):
+        """Import names from a nested list of modules to namespace"""
+        try:
+            mods = [import_module(mod) for mod in modules]
+        except ModuleNotFoundError as err:
+            raise ValueError(err)
+
+        try:
+            ns = SimpleNamespace(
+                **{
+                    name: getattr(mod, name)
+                    for mod in mods
+                    for name in mod.__all__
+                }
+            )
+        except AttributeError as err:
+            raise TypeError(f"non-conformant module: {err}")
+        else:
+            return ns
+
+    @property
+    def types(self):
+        if not hasattr(self, "_types"):
+            self._types = self._import(self._type_modules)
+        return self._types
+
+    @property
+    def validators(self):
+        if not hasattr(self, "_validators"):
+            self._validators = self._import(self._validator_modules)
+        return self._validators
+
+
+NS = _Names()
---
+++ pydc/types.py
@@ -0,0 +1,8 @@
+"""Types that maybe used in a config file"""
+
+# import standard library types for singleton namespace to import
+from builtins import bool, int, float, str
+from pathlib import Path
+
+
+__all__ = ["bool", "int", "float", "str", "Path"]
