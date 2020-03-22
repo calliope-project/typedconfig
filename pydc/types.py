4002f297fcfa776cc01a8981d44b7f5b78e4b4d2
brmanuel <manuel.broechin@hotmail.com>
2020-03-22 19:47:15 +0100

    pydc/types.py: numerals with units and file with subset specifier
    Implemented by Manuel Bröchin

--- pydc/types.py
+++ pydc/types.py
@@ -3,6 +3,42 @@
 # import standard library types for singleton namespace to import
 from builtins import bool, int, float, str
 from pathlib import Path
+from typing import Type
+
+from pydantic.dataclasses import dataclass as pydantic_dataclass
+from pydantic.types import confloat, conint, FilePath
 
 
-__all__ = ["bool", "int", "float", "str", "Path"]
+__all__ = [
+    "bool",
+    "int",
+    "float",
+    "str",
+    "Path",
+    "unitfloat",
+    "unitint",
+    "FileWithSubset",
+]
+
+
+def unitfloat(unit: str, **kwargs) -> Type[float]:
+    namespace = dict(unit=unit)
+    confloat_t = confloat(**kwargs)
+    return type("UnitFloat", (confloat_t,), namespace)
+
+
+def unitint(unit: str, **kwargs,) -> Type[int]:
+    namespace = dict(unit=unit)
+    conint_t = conint(**kwargs)
+    return type("UnitInt", (conint_t,), namespace)
+
+
+@pydantic_dataclass
+class FileWithSubset:
+    filepath: FilePath
+    column: str
+
+    @classmethod
+    def from_string(cls, string):
+        filepath, column = string.rsplit(":", 1)
+        return cls(filepath=filepath, column=column)
