a40562c9d89570da47ad4c1170a60108d6e131ef
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-03-23 16:38:42 +0100

    pydc/validators.py: a range check validator

---
+++ pydc/validators.py
@@ -0,0 +1,20 @@
+from typing import Callable, Dict, Sequence, get_type_hints
+
+from pydantic import validator
+
+__all__ = ["range_check"]
+
+
+# TODO: rewrite as a simple function, where the parameters are injected into
+# the closure in a generic factory method
+def range_check(
+    key: str, keys: Sequence[str], **opts
+) -> Dict[str, classmethod]:
+    @validator(key, **opts)
+    def _range_check(cls, _max, values):
+        """Validator function for a range config object"""
+        if keys[0] in values and values[keys[0]] > _max:
+            raise ValueError(f"bad range: {values[keys[0]]} > {_max}")
+        return _max
+
+    return {"range_check": _range_check}
