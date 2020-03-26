4cef88be7c05f830bc962f575dc3c46edcca873d
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-03-26 21:31:37 +0100

    test_helpers.py: fix namespace tests
    - create a new singleton for testing _Names
    - test if editing module list works
    - test if non-conformant module raises the right exception
    
    fixes #7

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,19 +1,29 @@
 import pytest
 
-from pydc.helpers import NS
+from pydc.helpers import _Names
 
 
-@pytest.mark.xfail(
-    reason="passes only when runs first, how to ensure order?", strict=False
-)
 def test_nonexistent_module():
+    # Importing the singleton namespace and misconfiguring it meant all
+    # following tests would be affected, and those depending on it would fail.
+    # So create an isolated singleton object.
+    NS = _Names()
     NS._type_modules = ("nonexistent",)
     with pytest.raises(ValueError):
         NS.types
 
 
-@pytest.mark.skip(reason="not sure how to generate a module on the fly")
+def test_edit_module_list():
+    NS = _Names()
+    NS._type_modules = ("typing",)
+    assert hasattr(NS.types, "List")
+    assert not hasattr(NS.types, "PositiveInt")
+
+
 def test_nonconformant_module():
-    # modules with custom types and validators are required to have __all__
-    # defined.  The names defined there are imported into the namespace.
-    pass
+    # modules in the module list are required to have __all__ defined; simulate
+    # a non-conformant module by importing one of the internal modules
+    NS = _Names()
+    NS._type_modules = ("pydc.helpers",)
+    with pytest.raises(TypeError):
+        NS.types
