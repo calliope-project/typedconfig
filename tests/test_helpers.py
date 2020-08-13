5b27d70fd22a89b29c171c3c1cec7573dfbb6293
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-08-13 16:46:30 +0200

    test_helpers: test for merging configs
    fixes #5

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,9 @@
-import pytest
+from io import StringIO
 
-from typedconfig.helpers import _Names
+import pytest
+import yaml
+
+from typedconfig.helpers import _Names, merge_dicts, merge_rules
 
 
 def test_nonexistent_module():
@@ -39,3 +42,33 @@ def test_reload():
     # pydantic.types is not loaded, so PositiveInt isn't available
     assert hasattr(NS.types, "List")
     assert not hasattr(NS.types, "PositiveInt")
+
+
+def test_merge():
+    d1 = {"a": 1, "b": {"c": 3, "d": 4}, "e": True}
+    d2 = {"c": 3, "b": {"e": 5, "d": 40}, "e": {"g": True, "h": "foo"}}
+    # - b.d tests overwriting values
+    # - e & b.d tests order
+    # - e & e.* tests adding new sub-keys
+    expected = {
+        "a": 1,
+        "b": {"c": 3, "d": 40, "e": 5},
+        "e": {"g": True, "h": "foo"},
+        "c": 3,
+    }
+    result = merge_dicts([d1, d2])
+    assert result == expected
+    # check order
+    assert result.keys() == expected.keys()
+    assert result["b"].keys() == expected["b"].keys()
+
+    streams = [StringIO(), StringIO()]
+    for i, d in enumerate((d1, d2)):
+        yaml.dump(d, streams[i])
+        streams[i].seek(0)
+
+    result = merge_rules(streams, yaml.safe_load)
+    assert result == expected
+    # check order
+    assert result.keys() == expected.keys()
+    assert result["b"].keys() == expected["b"].keys()
